ed10e4fe657d72f7f7e71cf586ada2f54e44e186
jchessma <josh.chessman@gmail.com>
2025-10-16 17:07:37 -0400

    Update scrapeProton.sh

--- scrapeProton.sh
+++ scrapeProton.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-tmpIPFile="/tmp/tmpIPFile.txt"
+tmpIPFile="best_server_ip.txt" # Change this to whatever the filename in the primary script is
 
 cd /home/josh/dev/pfsense-proton
 source venv/bin/activate
 
-python3 /home/josh/dev/pfsense-proton/scrape-ng.py > $tmpIPFile
+python3 /home/josh/dev/pfsense-proton/scrape-ng.py
 
 scp $tmpIPFile admin@<IP/FQDN of pfSense>:
